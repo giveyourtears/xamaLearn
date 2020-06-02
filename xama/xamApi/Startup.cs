f212a338dec1307caffdb93dd6c54ce67f618db8
Artem <giveyourtears@gmail.com>
2020-06-02 19:53:09 +0000

    change startup file

--- xama/xamApi/Startup.cs
+++ xama/xamApi/Startup.cs
@@ -2,14 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using xamApi.Models;
 
 namespace xamApi
 {
@@ -25,6 +28,14 @@ namespace xamApi
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      string connection = Configuration.GetConnectionString("DefaultConnection");
+      services.AddDbContext<UserContext>(options => options.UseSqlServer(connection));
+
+      services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options => //CookieAuthenticationOptions
+        {
+          options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+        });
       services.AddControllers();
     }
 
@@ -38,13 +49,20 @@ namespace xamApi
 
       app.UseHttpsRedirection();
 
+      app.UseStaticFiles();
+
       app.UseRouting();
 
       app.UseAuthorization();
 
+      app.UseAuthentication();
+      app.UseAuthorization();
+
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapControllers();
+        endpoints.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}");
       });
     }
   }
