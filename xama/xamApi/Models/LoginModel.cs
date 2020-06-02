5ef30f9277342a7af2cac798ad8ffa9730892ab3
Artem <giveyourtears@gmail.com>
2020-06-02 20:00:05 +0000

    re-create models with types

---
+++ xama/xamApi/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace xamApi.Models
+{
+  public class LoginModel
+  {
+    [Required(ErrorMessage = "Не указан логин")]
+    public string Login { get; set; }
+    [Required(ErrorMessage = "Не указан пароль")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+  }
+}
