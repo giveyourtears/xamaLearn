ff5643c82e4b8add8ad24f5cfa24b5e0e04ea7a2
Artem <giveyourtears@gmail.com>
2020-06-02 19:41:43 +0000

    add dbcontext model and connection string

---
+++ xama/xamApi/Models/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace xamApi.Models
+{
+  public class UserContext : DbContext
+  {
+    public DbSet<UserModel> Users { get; set; }
+
+    public UserContext(DbContextOptions<UserContext> options) : base(options)
+    {
+      Database.EnsureCreated();
+    }
+  }
+}
--- xama/xamApi/Models/UserModel.cs
+++ xama/xamApi/Models/UserModel.cs
@@ -5,7 +5,11 @@ using System.Threading.Tasks;
 
 namespace xamApi.Models
 {
-  public class User
+  public class UserModel
   {
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
   }
 }
