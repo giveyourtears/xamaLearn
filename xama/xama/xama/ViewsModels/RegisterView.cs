b892e8db1f65dd70521333caad018598756d7768
Artem <giveyourtears@gmail.com>
2020-06-17 23:32:29 +0300

    fixes with changeEventHandler

--- xama/xama/xama/ViewsModels/RegisterView.cs
+++ xama/xama/xama/ViewsModels/RegisterView.cs
@@ -1,12 +1,12 @@
-﻿using System.Windows.Input;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using xama.Services;
-using xama.Views;
 using Xamarin.Forms;
 
 namespace xama.ViewsModels
 {
-  class RegisterView
+  class RegisterView : INotifyPropertyChanged
   {
     Service api = new Service();
     public string Username { get; set; }
@@ -14,18 +14,42 @@ namespace xama.ViewsModels
     public string FirstName { get; set; }
     public string LastName { get; set; }
 
-    public string Message { get; set; }
+    private string _message;
+
+    public string Message
+    {
+      get => _message;
+      set
+      {
+        if (_message == value || value == null)
+          return;
+        _message = value;
+        OnPropertyChanged();
+      }
+    }
 
     public ICommand RegisterAction
     {
       get
       {
-        return new Command(async() =>
+        return new Command(async () =>
         {
           var isSuccess = await api.Register(FirstName, LastName, Username, Password);
           Message = isSuccess ? "Registered successfully" : "Retry again";
         });
       }
     }
+
+
+    #region INotifyPropertyChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+      var changed = PropertyChanged;
+
+      changed?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
   }
 }
