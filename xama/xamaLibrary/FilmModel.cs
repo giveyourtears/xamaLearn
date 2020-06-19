eb7efe3e6e109eb5b80e4fb558b94e44890472df
Artem <giveyourtears@gmail.com>
2020-06-19 16:45:45 +0300

    add film model

---
+++ xama/xamaLibrary/FilmModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamaLibrary
+{
+    public class FilmModel
+    {
+        public int Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        int Duration { get; set; }
+        string Genre { get; set; }
+        DateTime DatePremier { get; set; }
+        string Starring { get; set; }
+    }
+}
